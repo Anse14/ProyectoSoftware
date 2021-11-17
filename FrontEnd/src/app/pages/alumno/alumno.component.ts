3674650d75c3876acce061d3a2c309ff9b63c2b2
ivhyarleth <ivhyarleth@github.com>
2021-11-17 17:36:24 -0500

    grafico estadistica

--- FrontEnd/src/app/pages/alumno/alumno.component.ts
+++ FrontEnd/src/app/pages/alumno/alumno.component.ts
@@ -3,104 +3,104 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-alumno',
   templateUrl: './alumno.component.html',
-  styleUrls: ['./alumno.component.scss']
+  styleUrls: ['./alumno.component.scss'],
 })
 export class AlumnoComponent implements OnInit {
-  Alumno = "Alexender Schidmt"
+  Alumno = 'Alexender Schidmt';
 
   cursos = [
     {
-      titulo: "CÓDIGO - Curso 1",
+      titulo: 'CÓDIGO - Curso 1',
       rubrica: {
-        titulo: "SISTEMA DE EVALUACIÓN",
-        titulos: ["EXAMEN", "NOTA"],
+        titulo: 'SISTEMA DE EVALUACIÓN',
+        titulos: ['EXAMEN', 'NOTA'],
         elementos: [
-          ["PC1", 12],
-          ["PC2", 15],
-          ["PC3", 10.5],
-          ["PC4", 20],
-          ["P", "-"],
-        ]
-      }
+          ['PC1', 12],
+          ['PC2', 15],
+          ['PC3', 10.5],
+          ['PC4', 20],
+          ['P', '-'],
+        ],
+      },
     },
     {
-      titulo: "CÓDIGO - Curso 2",
+      titulo: 'CÓDIGO - Curso 2',
       rubrica: {
-        titulo: "SISTEMA DE EVALUACIÓN",
-        titulos: ["EXAMEN", "NOTA"],
+        titulo: 'SISTEMA DE EVALUACIÓN',
+        titulos: ['EXAMEN', 'NOTA'],
         elementos: [
-          ["PC1", 12],
-          ["PC2", 15],
-          ["PC3", 10.5],
-          ["PC4", 20],
-          ["P", "-"],
-        ]
-      }
+          ['PC1', 12],
+          ['PC2', 15],
+          ['PC3', 10.5],
+          ['PC4', 20],
+          ['P', '-'],
+        ],
+      },
     },
     {
-      titulo: "CÓDIGO - Curso 3",
+      titulo: 'CÓDIGO - Curso 3',
       rubrica: {
-        titulo: "SISTEMA DE EVALUACIÓN",
-        titulos: ["EXAMEN", "NOTA"],
+        titulo: 'SISTEMA DE EVALUACIÓN',
+        titulos: ['EXAMEN', 'NOTA'],
         elementos: [
-          ["PC1", 12],
-          ["PC2", 15],
-          ["PC3", 10.5],
-          ["PC4", 20],
-          ["P", "-"],
-        ]
-      }
+          ['PC1', 12],
+          ['PC2', 15],
+          ['PC3', 10.5],
+          ['PC4', 20],
+          ['P', '-'],
+        ],
+      },
     },
     {
-      titulo: "CÓDIGO - Curso 4",
+      titulo: 'CÓDIGO - Curso 4',
       rubrica: {
-        titulo: "SISTEMA DE EVALUACIÓN",
-        titulos: ["EXAMEN", "NOTA"],
+        titulo: 'SISTEMA DE EVALUACIÓN',
+        titulos: ['EXAMEN', 'NOTA'],
         elementos: [
-          ["PC1", 12],
-          ["PC2", 15],
-          ["PC3", 10.5],
-          ["PC4", 20],
-          ["P", "-"],
-        ]
-      }
+          ['PC1', 12],
+          ['PC2', 15],
+          ['PC3', 10.5],
+          ['PC4', 20],
+          ['P', '-'],
+        ],
+      },
     },
     {
-      titulo: "CÓDIGO - Curso 5",
+      titulo: 'CÓDIGO - Curso 5',
       rubrica: {
-        titulo: "SISTEMA DE EVALUACIÓN",
-        titulos: ["EXAMEN", "NOTA"],
+        titulo: 'SISTEMA DE EVALUACIÓN',
+        titulos: ['EXAMEN', 'NOTA'],
         elementos: [
-          ["PC1", 12],
-          ["PC2", 15],
-          ["PC3", 10.5],
-          ["PC4", 20],
-          ["P", "-"],
-        ]
-      }
+          ['PC1', 12],
+          ['PC2', 15],
+          ['PC3', 10.5],
+          ['PC4', 20],
+          ['P', '-'],
+        ],
+      },
     },
     {
-      titulo: "CÓDIGO - Curso 6",
+      titulo: 'CÓDIGO - Curso 6',
       rubrica: {
-        titulo: "SISTEMA DE EVALUACIÓN",
-        titulos: ["EXAMEN", "NOTA"],
+        titulo: 'SISTEMA DE EVALUACIÓN',
+        titulos: ['EXAMEN', 'NOTA'],
         elementos: [
-          ["PC1", 12],
-          ["PC2", 15],
-          ["PC3", 10.5],
-          ["PC4", 20],
-          ["P", "-"],
-        ]
-      }
+          ['PC1', 12],
+          ['PC2', 15],
+          ['PC3', 10.5],
+          ['PC4', 20],
+          ['P', '-'],
+        ],
+      },
     },
-  ]
+  ];
 
-  type ='bar';
+  type = 'bar';
 
-  dati=[
+  dati = [
     {
-      rubrica:{
-        titulo: "COMPETENCIAS",
+      rubrica: {
+        titulo: 'COMPETENCIAS',
         labels: ['PC1', 'PC2', 'PC3', 'PC4', 'P'],
         datasets: [
           {
@@ -111,21 +111,21 @@ export class AlumnoComponent implements OnInit {
               'rgba(255, 159, 64, 0.2)',
               'rgba(255, 205, 86, 0.2)',
               'rgba(75, 192, 192, 0.2)',
-              'rgba(54, 162, 235, 0.2)'
+              'rgba(54, 162, 235, 0.2)',
             ],
             borderColor: [
               'rgb(255, 99, 132)',
               'rgb(255, 159, 64)',
               'rgb(255, 205, 86)',
               'rgb(75, 192, 192)',
-              'rgb(54, 162, 235)'
+              'rgb(54, 162, 235)',
             ],
-            borderWidth: 1
+            borderWidth: 1,
           },
         ],
-      }
+      },
     },
-  ]
+  ];
 
   data = {
     labels: ['PC1', 'PC2', 'PC3', 'PC4', 'P'],
@@ -138,30 +138,26 @@ export class AlumnoComponent implements OnInit {
           'rgba(255, 159, 64, 0.2)',
           'rgba(255, 205, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
-          'rgba(54, 162, 235, 0.2)'
+          'rgba(54, 162, 235, 0.2)',
         ],
         borderColor: [
           'rgb(255, 99, 132)',
           'rgb(255, 159, 64)',
           'rgb(255, 205, 86)',
           'rgb(75, 192, 192)',
-          'rgb(54, 162, 235)'
+          'rgb(54, 162, 235)',
         ],
-        borderWidth: 1
+        borderWidth: 1,
       },
     ],
   };
 
-  
   options = {
     responsive: true,
     maintainAspectRatio: true,
   };
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
