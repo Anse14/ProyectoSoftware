04d4ca9dd0bd6d7be10c6a8a52f6eeb1f0664e65
Anxxy <assm14401@gmail.com>
2021-11-17 03:06:05 -0500

    owo

--- BackEnd/src/main/java/com/utec/software/model/Rol.java
+++ BackEnd/src/main/java/com/utec/software/model/Rol.java
@@ -1,18 +1,28 @@
 package com.utec.software.model;
 
 import com.utec.software.model.enums.RolEnum;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
-public class Rol extends PanacheEntity {
+public class Rol extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
     private RolEnum tipo;
 
     @OneToMany(targetEntity = User.class)
