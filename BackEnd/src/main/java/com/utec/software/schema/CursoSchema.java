3ae23a12d0492d05ddf28a010df66d5b7f853e07
Andres <40176339+Andres20-Utec@users.noreply.github.com>
2021-11-24 17:08:11 -0500

    schema

---
+++ BackEnd/src/main/java/com/utec/software/schema/CursoSchema.java
@@ -0,0 +1,20 @@
+package com.utec.software.schema;
+
+import com.utec.software.model.enums.RolEnum;
+
+public class CursoSchema {
+    public CursoSchema() {}
+
+    private String codigo;
+    private String titulo;
+    private String ciclo;
+    private String semestre;
+    public CursoSchema(String codigo, String titulo, String ciclo, String semestre) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.ciclo = ciclo;
+        this.semestre = semestre;
+    }
+
+
+}
