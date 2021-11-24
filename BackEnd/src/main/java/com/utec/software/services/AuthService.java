137c3a8c8ca6beac3fa9cf8964a72aafa35650a9
Anxxy <assm14401@gmail.com>
2021-11-24 15:13:02 -0500

    Uff

--- BackEnd/src/main/java/com/utec/software/services/AuthService.java
+++ BackEnd/src/main/java/com/utec/software/services/AuthService.java
@@ -32,7 +32,7 @@ public class AuthService {
         return JWT.create()
                 .withSubject(subject)
                 .withIssuedAt(Date.from(now))
-                .withExpiresAt(Date.from(now.plus(1, ChronoUnit.MINUTES)))
+                .withExpiresAt(Date.from(now.plus(24, ChronoUnit.HOURS)))
                 .sign(algorithm);
     }
 
